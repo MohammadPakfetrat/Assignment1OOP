8495123350119cbb3e11e70cc48a0efc13aa3eb4
MohammadPakfetrat <MohammadPakfetrat@hotmail.com>
2025-10-15 11:42:48 +0200

    Jag har gjort Atributer, konstruktor och metoder i klassen JobApplication

---
+++ JobApplication.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Assignment1OOP
+{
+    public class JobApplication
+    {
+        // atributer
+        public string CompanyName { get; set; }
+        public string PositionTitle { get; set; }
+        public enum status { Applied, Interviewing, Offered, Rejected }
+        public DateTime ApplicationDate { get; set; }
+        public DateTime? ResponseDate { get; set; }
+        public int SalaryExpectation { get; set; }
+
+        // konstruktor
+
+        public JobApplication(string companyName, string positionTitle, DateTime applicationDate, DateTime? responseDate, int salaryExpectation)
+        {
+            CompanyName = companyName;
+            PositionTitle = positionTitle;
+            ApplicationDate = applicationDate;
+            SalaryExpectation = salaryExpectation;
+            ResponseDate = responseDate;
+        }
+
+        // metoder
+
+        public int GetdaysSinceApplied()
+        {
+            return (DateTime.Now - ApplicationDate).Days;
+        }
+
+        public string GetSummery()
+        {
+            return $"Company: {CompanyName}, Position: {PositionTitle}, Applied on: {ApplicationDate:yyyy-MM-dd}, Salary Expectation: {SalaryExpectation} USD";
+        }
+    }
+}
